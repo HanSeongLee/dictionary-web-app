1e443a1ace569edcb25e84231bff67135b40a291
HanSeongLee <jkhanseong@gmail.com>
2023-01-28 19:53:33 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import Header from 'components/Header';
+import Container from 'components/Container';
+import DictionarySearchContainer from 'containers/DictionarySearchContainer';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
+    const router = useRouter();
+    const { q } = router.query;
+
+    return (
+        <>
+            <main className={styles.main}>
+                <Header />
+                <Container>
+                    <DictionarySearchContainer className={styles.dictionarySearchContainer}
+                                               query={q as string}
+                    />
+                </Container>
+            </main>
+        </>
+    );
+};
 
 export default Home
